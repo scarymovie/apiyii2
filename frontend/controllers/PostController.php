9c45487b9de8e3cc3436103e26404d3aa0723411
scarymovie <81684836+scarymovie@users.noreply.github.com>
2021-08-17 16:06:45 +0300

    added functions with access_token

--- frontend/controllers/PostController.php
+++ frontend/controllers/PostController.php
@@ -3,9 +3,36 @@
 namespace frontend\controllers;
 
 use common\models\Post;
+use yii\filters\auth\CompositeAuth;
+use yii\filters\auth\HttpBearerAuth;
 use yii\rest\ActiveController;
+use yii\web\ForbiddenHttpException;
 
 class PostController extends ActiveController
 {
 public $modelClass = Post::class;
+
+public function behaviors()
+{
+    $behaviors = parent::behaviors();
+    $behaviors['authenticator']['only'] = ['create','update','delete'];
+    $behaviors['authenticator']['authMethods'] = [
+        HttpBearerAuth::class
+    ];
+    return $behaviors;
+}
+
+
+    /**
+     * @param string $action
+     * @param Post $model
+     * @param array $params
+     * @throws ForbiddenHttpException
+     */
+    public function checkAccess($action, $model = null, $params = [])
+{
+    if (in_array($action,['update','delete'])&& $model->created_by !==\Yii::$app->user->id){
+        throw new ForbiddenHttpException("У вас нет доступа");
+    }
+}
 }
